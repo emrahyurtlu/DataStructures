450b80cb974d0398a07369120eab92149c2beae8
Emrah Yurtlu <emrahyurtlu@gmail.com>
2022-06-18 16:39:47 +0300

    Commit

---
+++ LinkedList/ILinkedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LinkedList
+{
+    public interface ILinkedList<T>
+    {
+        void AddFirst(T element);
+
+        void AddLast(T element);
+
+        void Remove(T element);
+
+        int Length();
+
+        bool IsEmpty();
+
+        Node<T>? Has(T element);
+
+        List<T> ToList();
+
+        T Head();
+
+        T Tail();
+
+        void Print();
+    }
+}
+
