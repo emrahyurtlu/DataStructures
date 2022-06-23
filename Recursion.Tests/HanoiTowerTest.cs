96b5f1ce5edfd71d3af15e89853ffbbfa91a5345
Emrah Yurtlu <emrahyurtlu@gmail.com>
2022-06-23 08:15:42 +0300

    Hanoi towers problem are implemented but not tested.

---
+++ Recursion.Tests/HanoiTowerTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit.Abstractions;
+
+namespace Recursion.Tests;
+
+public class HanoiTowerTest
+{
+    private readonly ITestOutputHelper _output;
+
+    public HanoiTowerTest(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public void TestOneWithABC()
+    {
+        var hanoi = new TowersOfHanoi();
+        hanoi.Print(1, 'A', 'B', 'C');
+        var expected = "A --> B";
+        // _output.WriteLine(expected);
+
+        Assert.True(false);
+    }
+}
--- Recursion/TowersOfHanoi.cs
+++ Recursion/TowersOfHanoi.cs
@@ -2,5 +2,17 @@
 
 public class TowersOfHanoi
 {
-    // TODO: this class will be implemented.
+    public void Print(int number, char x, char y, char z)
+    {
+        if (number == 1)
+        {
+            Console.WriteLine(x + " --> " + y);
+        }
+        else
+        {
+            Print(number - 1, x, z, y);
+            Console.WriteLine(x + " --> " + y);
+            Print(number - 1, z, y, x);
+        }
+    }
 }
